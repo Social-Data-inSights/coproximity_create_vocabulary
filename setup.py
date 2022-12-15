8af27dfb34dd4200bd1dba63178483d1f16a3887
MatthieuDev <matthieu.devaux@unil.ch>
2022-12-15 16:00:33 +0100

    requirements good

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
-setup(name='coproximity_create_vocabulary',
+setup(
+      name='coproximity_create_vocabulary',
       version='0.1',
       description='The funniest joke in the world',
       url='http://github.com/storborg/funniest',
@@ -8,4 +9,6 @@ setup(name='coproximity_create_vocabulary',
       author_email='matthieu.devaux@alumni.epfl.ch',
       license='MIT',
       packages=['coproximity_create_vocabulary'],
-      zip_safe=False)
+      install_requires=['flask', 'spacy', 'requests', 'fasttext', 'mwparserfromhell'],
+      zip_safe=False
+)
