f2bd699ba49459ee187ea046992b47ddc43d2ef8
MatthieuDev <matthieu.devaux@unil.ch>
2023-01-18 18:03:51 +0100

    send_static_share: docker add /

--- docker/junipero_update_vocab_and_send.sh
+++ docker/junipero_update_vocab_and_send.sh
@@ -3,5 +3,5 @@ mkdir ../vocabulary
 docker build -f dockerfile_send_data -t vocab_and_send:fr --build-arg spacy_model=fr_core_news_lg --build-arg additional_arg="-p fr -l french --disable_tag tagger_parser_ner --fasttext_model fr" --no-cache .
 #docker build -f dockerfile_send_data -t vocab_and_send:en --build-arg spacy_model=en_core_web_lg --build-arg additional_arg="-p en -l english --disable_tag parser_ner --fasttext_model en" --no-cache .
 
-docker run --rm -v "/home/matthieu/create_vocab/coproximity_create_vocabulary/vocabulary":/vm/data/ vocab_and_send:fr
-#docker run --rm -v "/home/matthieu/create_vocab/coproximity_create_vocabulary/vocabulary":/vm/data/ vocab_and_send:en
+docker run --rm -v "/home/matthieu/create_vocab/coproximity_create_vocabulary/vocabulary/":/vm/data/ vocab_and_send:fr
+#docker run --rm -v "/home/matthieu/create_vocab/coproximity_create_vocabulary/vocabulary/":/vm/data/ vocab_and_send:en
